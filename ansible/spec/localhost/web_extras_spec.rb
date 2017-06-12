cc998b20f8a7a983e6dae264e0135446be41eb94
James McCallum <jamesgmccallum@gmail.com>
2017-06-12 21:23:12 +1000

    install web dev extras

---
+++ ansible/spec/localhost/web_extras_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe package('cordova') do
+  it { should be_installed.by(:npm) }
+end
+
+describe command('cordova -v') do
+  its(:exit_status) { should eq 0 }
+end
+
+describe package('ionic') do
+  it { should be_installed.by(:npm) }
+end
+
+describe command('ionic -v') do
+  its(:exit_status) { should eq 0 }
+end
+
+describe package('grunt') do
+  it { should be_installed.by(:npm) }
+end
+
+describe command('grunt --version') do
+  its(:exit_status) { should eq 0 }
+end
+
+describe package('bower') do
+  it { should be_installed.by(:npm) }
+end
+
+describe command('bower -v') do
+  its(:exit_status) { should eq 0 }
+end
+
+describe package('gulp') do
+  it { should be_installed.by(:npm) }
+end
+
+describe command('gulp --version') do
+  its(:exit_status) { should eq 0 }
+end
