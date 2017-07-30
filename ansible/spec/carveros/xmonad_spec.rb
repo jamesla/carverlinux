b4be137c19afa092394c0f75597220f8b0485816
James McCallum <jamesgmccallum@gmail.com>
2017-07-30 20:09:36 +1000

    add test for demnu

--- ansible/spec/carveros/xmonad_spec.rb
+++ ansible/spec/carveros/xmonad_spec.rb
@@ -4,10 +4,10 @@ describe package('xmonad') do
   it { should be_installed }
 end
 
-# describe package('suckless-tools') do
-#   it { should be_installed }
-# end
+describe package('suckless-tools') do
+  it { should be_installed }
+end
 
-# describe command('dmenu -v') do
-#   its(:exit_status) { should eq 0 }
-# end
+describe command('dmenu -v') do
+  its(:exit_status) { should eq 0 }
+end
