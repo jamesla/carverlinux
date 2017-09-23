4e7e5bf20a5bfaebf83897f4042db73b075a2788
James McCallum <jamesgmccallum@gmail.com>
2017-09-22 22:45:02 -0700

    fix rupocop, tmux

--- ansible/spec/carveros/tmux_spec.rb
+++ ansible/spec/carveros/tmux_spec.rb
@@ -1,9 +1,13 @@
 require 'spec_helper'
 
-describe package('tmux') do
-  it { should be_installed }
+describe file('/usr/bin/tmux') do
+  it { should exist }
 end
 
 describe file('/home/vagrant/.tmux.conf') do
   it { should be_symlink }
+end
+
+describe command('tmux -V') do
+  its(:exit_status) { should eq 0 }
 end
