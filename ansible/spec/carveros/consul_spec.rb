a70ea32c23d4a891b359f59cf7475759dc6f0473
James McCallum <jamesgmccallum@gmail.com>
2017-11-19 11:49:42 +1100

    remove duplicate vagrant tests

--- ansible/spec/carveros/consul_spec.rb
+++ ansible/spec/carveros/consul_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
-describe package('vagrant') do
+describe package('consul') do
   it { should be_installed }
 end
 
-describe command('vagrant -v') do
+describe command('consul -v') do
   its(:exit_status) { should eq 0 }
 end
