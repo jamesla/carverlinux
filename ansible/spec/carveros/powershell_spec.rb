0081a71c7d4e44e55dd477af4d58f033343df87d
James McCallum <jamesgmccallum@gmail.com>
2018-07-02 20:18:53 +1000

    remove powershell

--- ansible/spec/carveros/powershell_spec.rb
+++
@@ -1,9 +0,0 @@
-# require 'spec_helper'
-
-# describe package('powershell') do
-#   it { should be_installed }
-# end
-
-# describe command('pwsh -v') do
-#   its(:exit_status) { should eq 0 }
-# end
