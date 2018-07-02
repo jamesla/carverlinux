1ee86464fb6bd23be1ce20ce5255079972c98320
James McCallum <jamesgmccallum@gmail.com>
2018-07-02 18:30:18 +1000

    Bionic upgrade (#11)
    * update to bionic beaver and ruby 2.5
    
    * upgrade bionic
    
    * remove powershell
    
    * building bionic

--- ansible/spec/carveros/powershell_spec.rb
+++ ansible/spec/carveros/powershell_spec.rb
@@ -1,9 +1,9 @@
-require 'spec_helper'
+# require 'spec_helper'
 
-describe package('powershell') do
-  it { should be_installed }
-end
+# describe package('powershell') do
+#   it { should be_installed }
+# end
 
-describe command('pwsh -v') do
-  its(:exit_status) { should eq 0 }
-end
+# describe command('pwsh -v') do
+#   its(:exit_status) { should eq 0 }
+# end
