9af4b1a4db76c1f7e7a0b88ca8901bfde45ba283
James McCallum <jamesgmccallum@gmail.com>
2019-03-24 16:44:38 +1100

    upgrade st to 0.8.2, alpha patch and upgrade xmonad to support spacing

--- ansible/roles/desktop/files/xmonad.hs
+++ ansible/roles/desktop/files/xmonad.hs
@@ -4,6 +4,7 @@ import qualified XMonad.Util.CustomKeys as C
 import qualified Data.Map as M
 import XMonad.Actions.SpawnOn
 import XMonad.Util.EZConfig
+import XMonad.Layout.Spacing
 
 main :: IO ()
 main = xmonad $ defaultConfig
@@ -11,6 +12,8 @@ main = xmonad $ defaultConfig
     , terminal         = "st -e tmux new-session -A -s carver"
     , normalBorderColor  = "#000000"
     , focusedBorderColor = "#000000"
+    , layoutHook = spacingRaw True (Border 0 10 10 10) True (Border 10 10 10 10) True $
+        layoutHook def
     , startupHook = do
-        spawn "st -e tmux new-session -A -s carver"
+      spawn "st -e tmux new-session -A -s carver"
     }
