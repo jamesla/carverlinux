57b56c9c1fdfa30a088150ca1e0cfc21df05da5c
James McCallum <jamesgmccallum@gmail.com>
2019-08-22 22:09:51 +1000

    Feature/add kube tools (#34)
    * wip
    
    * add kube tools
    
    * PR
    
    * PR
    
    * fix piping
    
    * update to version 3 of ubuntu 1804
    
    * working on new dind builder

---
+++ specs/test_random.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+files = %w[
+  /usr/local/bin/oc
+  /usr/local/bin/minishift
+  /usr/local/bin/istioctl
+  /usr/local/bin/glooctl
+  /snap/bin/helm
+]
+
+files.each do |f|
+  describe file(f) do
+    it { should exist }
+    it { should be_executable }
+  end
+end
+
+commands = [
+  'helm --help',
+  'oc version',
+  'istioctl --help',
+  'glooctl --help',
+  'minishift version'
+]
+
+commands.each do |c|
+  describe command(c) do
+    its(:exit_status) { should eq 0 }
+  end
+end
