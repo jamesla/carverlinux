5eddadf7c4b85c12703af117ac22651108bab073
James McCallum <jamesgmccallum@gmail.com>
2020-02-24 07:45:22 +1100

    fix: remove old vim tests

--- specs/test_vim.rb
+++ specs/test_vim.rb
@@ -1,13 +1,5 @@
 # frozen_string_literal: true
 
-describe file('/home/vagrant/.vimrc') do
-  it { should exist }
-end
-
-describe file('/home/vagrant/.vim/bundle/Vundle.vim') do
-  it { should exist }
-end
-
 describe package('vim') do
   it { should be_installed }
 end
