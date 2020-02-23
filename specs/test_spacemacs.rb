1a8645f1948ce64d0e495cfe641b7c926d13aa58
James McCallum <jamesgmccallum@gmail.com>
2020-02-24 08:57:42 +1100

    fix: remove npm tests

--- specs/test_spacemacs.rb
+++ specs/test_spacemacs.rb
@@ -11,6 +11,21 @@ apt_packages.each do |p|
   end
 end
 
+<<<<<<< HEAD
+=======
+# npm_packages = %w[
+  # tern
+  # js-yaml
+  # vmd
+# ]
+#
+# npm_packages.each do |p|
+  # describe npm(p) do
+    # it { should be_installed }
+  # end
+# end
+
+>>>>>>> fix: remove npm tests
 describe file('/home/vagrant/.emacs.d') do
   it { should be_directory }
 end
