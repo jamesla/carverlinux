19036b51928a9aab296c967992c96fa40b0dc186
jamesla <jamesgmccallum@gmail.com>
2024-06-02 19:38:27 +1200

    chore: nixos 24.05

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -4,9 +4,9 @@
   '';
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     unstablepkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
